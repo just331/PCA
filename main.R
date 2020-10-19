81e57b6452a6ccce5392f72eede4aeb05350a253
Justin <justinr331@gmail.com>
2020-10-19 18:35:38 -0500

    Complete simple pca example

--- main.R
+++ main.R
@@ -28,3 +28,28 @@ c3 <- cov(y1,y1)
 c4 <- cov(y1, x1)
 m <-matrix(c(c2, c1, c4, c3), nrow = 2, ncol = 2, byrow = TRUE, dimnames = list(c("x", "y"), c("x", "y")))
 m
+
+
+evec <- eigen(m)
+evec
+
+pc1 <- x1 * evec$vectors[1,1] + y1 * evec$vectors[2,1]
+pc1
+
+pc2 <- x1 * evec$vectors[1,2] + y1 * evec$vectors[2,2]
+pc2
+
+data.frame(PC1=pc1, PC2=pc2)
+plot(pc1, pc2, pch=19)
+
+
+data <- data.frame(x,y)
+data.pca <- prcomp(data)
+data.pca
+
+names(data.pca)
+data.pca$x
+plot(data.pca$x[,1], data.pca$x[,2], pch=19)
+
+eigen(m)
+data.pca
